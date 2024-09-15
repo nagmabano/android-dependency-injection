e0f0a8b57343a8acba573b533464ca64364780f5
nagma <bano.nagma.1@gmail.com>
2024-09-15 23:00:45 +0530

    created hilt module

---
+++ app/src/main/java/com/learning/android/di/HiltModule.kt
@@ -0,0 +1,16 @@
+package com.learning.android.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object HiltModule {
+
+    @Provides
+    fun provideCoffees(): Coffee {
+        return Coffee()
+    }
+}
